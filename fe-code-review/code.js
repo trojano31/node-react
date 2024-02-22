5dd05ff54bbab25c86451f0544e22578d38ec6ed
trojan <patryk.trojanowski98@gmail.com>
2024-02-22 10:38:40 +0100

    code review

--- fe-code-review/code.js
+++ fe-code-review/code.js
@@ -1,58 +1,58 @@
-app.post('/api/extract', upload.single('file'), async (req, res) => {
+app.post('/api/extract', upload.single('file'), async (req, res) => { // i'd move all the logic to separate service/file as route should only handle simple calls. not whole logic
     logInfo('POST /api/extract',req.body);
     logInfo('FILE=',req.file);
 
-    if (req.body) {
+    if (req.body) { // personal preference but I'd instead check if there's no body and if so, exit function - i don't have to scroll to end of the route to see what will happen in that case
         const file = req.file;
         const requestID = req.body.requestID;
         const project = req.body.project;
         const idUser = req.body.userID;
         const user = await User.findOne(idUser);
 
-        if (requestID && project && idUser && user) {
+        if (requestID && project && idUser && user) { // this should be handled by validation middleware
             logDebug('User with role '+user.role, user);
             if (user.role === 'ADVISOR' || user.role.indexOf('ADVISOR') > -1)
                 return res.json({requestID, step: 999, status: 'DONE', message: 'Nothing to do for ADVISOR role'});
 
             /* reset status variables */
-            await db.updateStatus(requestID, 1, '');
+            await db.updateStatus(requestID, 1, ''); // Id consider making enum of the statuses so that its clear what status i am assigning, right now i dont know that this number means
 
             logDebug('CONFIG:', config.projects);
             if (project === 'inkasso' && config.projects.hasOwnProperty(project) && file) {
-                const hashSum = crypto.createHash('sha256');
-                const fileHash = idUser;
+                const hashSum = crypto.createHash('sha256'); // not used anywhere
+                const fileHash = idUser; // useless variable
                 const fileName = 'fullmakt';
                 const fileType = mime.getExtension(file.mimetype);
                 if (fileType !== 'pdf')
-                    return res.status(500).json({requestID, message: 'Missing pdf file'});
+                    return res.status(500).json({requestID, message: 'Missing pdf file'}); // can be refactored as util function as its used often and only differs in message
                 await db.updateStatus(requestID, 3, '');
 
-                const folder = `${project}-signed/${idUser}`;
+                const folder = `${project}-signed/${idUser}`; // id make it a function eg getFileLocation
                 logDebug('FILE2=', file);
-                await uploadToGCSExact(folder, fileHash, fileName, fileType, file.mimetype, file.buffer);
+                await uploadToGCSExact(folder, fileHash, fileName, fileType, file.mimetype, file.buffer); // what if it fails? the error should be handled
                 await db.updateStatus(requestID, 4, '');
                 const ret = await db.updateUploadedDocs(idUser, requestID, fileName, fileType, file.buffer);
                 logDebug('DB UPLOAD:', ret);
 
                 await db.updateStatus(requestID, 5, '');
 
-                let sent = true;
+                let sent = true; // not used anywhere
                 const debtCollectors = await db.getDebtCollectors();
                 logDebug('debtCollectors=', debtCollectors);
                 if (!debtCollectors)
-                    return res.status(500).json({requestID, message: 'Failed to get debt collectors'});
+                    return res.status(500).json({requestID, message: 'Failed to get debt collectors'}); // dunno what are the project specifications but there might be just no debt collectors and then the error message is misleading
 
-                if (!!(await db.hasUserRequestKey(idUser))) { //FIX: check age, not only if there's a request or not
+                if (!!(await db.hasUserRequestKey(idUser))) { //FIX: check age, not only if there's a request or not // kinda misleading comment, i dont see any age here
                     return res.json({requestID, step: 999, status: 'DONE', message: 'Emails already sent'});
                 }
 
                 const sentStatus = {};
-                for (let i = 0; i < debtCollectors.length ; i++) {
+                for (let i = 0; i < debtCollectors.length ; i++) { // refactor to forEach or for... of... function for better readability
                     await db.updateStatus(requestID, 10+i, '');
                     const idCollector = debtCollectors[i].id;
-                    const collectorName = debtCollectors[i].name;
+                    const collectorName = debtCollectors[i].name; // can be achieved with destructuring with less lines of code and better readability
                     const collectorEmail = debtCollectors[i].email;
-                    const hashSum = crypto.createHash('sha256');
+                    const hashSum = crypto.createHash('sha256'); //id make it a utility function
                     const hashInput = `${idUser}-${idCollector}-${(new Date()).toISOString()}`;
                     logDebug('hashInput=', hashInput);
                     hashSum.update(hashInput);
@@ -62,13 +62,13 @@ app.post('/api/extract', upload.single('file'), async (req, res) => {
                     const hash = Buffer.from(`${idUser}__${idCollector}`, 'utf8').toString('base64')
 
                     if (!!(await db.setUserRequestKey(requestKey, idUser))
-                        && !!(await db.setUserCollectorRequestKey(requestKey, idUser, idCollector))) {
+                        && !!(await db.setUserCollectorRequestKey(requestKey, idUser, idCollector))) { //id assign results to variables and check them in if statement
 
                         /* prepare email */
-                        const sendConfig = {
+                        const sendConfig = { // as a separate function
                             sender: config.projects[project].email.sender,
                             replyTo: config.projects[project].email.replyTo,
-                            subject: 'Email subject,
+                            subject: 'Email subject, // missing '
                             templateId: config.projects[project].email.template.collector,
                             params: {
                                 downloadUrl: `https://url.go/download?requestKey=${requestKey}&hash=${hash}`,
@@ -87,7 +87,7 @@ app.post('/api/extract', upload.single('file'), async (req, res) => {
                         }
 
                         /* send email */
-                        const resp = await email.send(sendConfig, config.projects[project].email.apiKey);
+                        const resp = await email.send(sendConfig, config.projects[project].email.apiKey); // i dont know here how is config handled but if apiKey is in plain text it should be loaded eg from env var
                         logDebug('extract() resp=', resp);
 
                         // update DB with result
@@ -113,7 +113,7 @@ app.post('/api/extract', upload.single('file'), async (req, res) => {
                 await db.updateStatus(requestID, 500, '');
 
                 /* prepare summary email */
-                const summaryConfig = {
+                const summaryConfig = { // move to separate function
                     //bcc: [{ email: 'tomas@inkassoregisteret.com', name: 'Tomas' }],
                     sender: config.projects[project].email.sender,
                     replyTo: config.projects[project].email.replyTo,
@@ -123,7 +123,7 @@ app.post('/api/extract', upload.single('file'), async (req, res) => {
                         collectors: sentStatus,
                     },
                     tags: ['summary'],
-                    to: [{ email: 'tomas@upscore.no' , name: 'Tomas' }], // FIXXX: config.projects[project].email.sender
+                    to: [{ email: 'tomas@upscore.no' , name: 'Tomas' }], // FIXXX: config.projects[project].email.sender // i dont understand - its not sender but receiver
                 };
                 logDebug('Summary config:', summaryConfig);
 
@@ -136,7 +136,7 @@ app.post('/api/extract', upload.single('file'), async (req, res) => {
             await db.updateStatus(requestID, 999, '');
             return res.json({requestID, step: 999, status: 'DONE', message: 'Done sending emails...'});
         } else
-            return res.status(500).json({requestID, message: 'Missing requried input (requestID, project, file)'});
+            return res.status(500).json({requestID, message: 'Missing requried input (requestID, project, file)'}); // wrong error for non existent user
     }
     res.status(500).json({requestID: '', message: 'Missing requried input (form data)'});
 });
